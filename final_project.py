94124527bd60c9b423badc0cbfe38c2119ed4e5a
Sahasra Chappidi <schappidi@ucsc-guest-169-233-125-238.ucsc.edu>
2026-07-22 08:11:24 -0700

    Removed live changes, it should show final state and number of steps

--- final_project.py
+++ final_project.py
@@ -4,34 +4,33 @@ import math
 import matplotlib.pyplot as plt
 
 # placeholders - change to answer questions
-N = 25 #length of the square lattice
+N = 10 #length of the square lattice
 T = 1 #temperature
 J = 1 #a constant that represents the strength and orientation of the interactions of spins
 h = 0 #external magnetic field parameter. if not implementing optionExtension, h = 0
 stabilityDeadband = 10*N**2
 
 squareLattice = np.array(np.random.choice([-1,1], size=(N,N)))
-previousSquareLattice = np.array(np.random.choice([-1,1], size=(N,N)))
+previousSquareLattice = np.copy(squareLattice)
 consecutiveEqualStates = 0
 
 def setPreviousSquareLattice():
     global squareLattice, previousSquareLattice
     previousSquareLattice = np.copy(squareLattice)
 
-def checkStability():
-    global squareLattice, previousSquareLattice, consecutiveEqualStates, stabilityDeadband
-    if np.array_equal(squareLattice,previousSquareLattice):
-        consecutiveEqualStates += 1
-    else:
+def checkStability(flipped):
+    global consecutiveEqualStates, stabilityDeadband
+    if flipped:
         consecutiveEqualStates = 0
-    setPreviousSquareLattice()
+    else:
+        consecutiveEqualStates += 1
     if consecutiveEqualStates>=stabilityDeadband:
         return True
     else:
         return False
 
 
-plt.ion()  # Turn on interactive mode
+#plt.ion()  # Turn on interactive mode
 # black is representing a spin of 1, white is -1
 fig, ax = plt.subplots(figsize=(6, 6))
 img = ax.imshow(squareLattice, cmap='binary', vmin=-1, vmax=1)
@@ -39,32 +38,35 @@ title_text = ax.set_title("Ising Model Simulation | Step: 0", fontsize=14)
 plt.title("Ising Model Simulation")
 plt.axis('off')
 
-
+flipped2 = False
 #implement Metropolis-Hastings algorithm
-for i in range(100000):
+for i in range(1000000):
    #choose a random point on the lattice
-   if checkStability():
+   if checkStability(flipped2):
        print("The Ising Model is stable at Step:",i)
        break
-   
+   current_flip = False
    r = random.randint(0,(N-1))
    c = random.randint(0,(N-1))
    energy = 4*J*squareLattice[r][c]*(squareLattice[(r+1) % N][c]+squareLattice[(r-1) % N][c]+squareLattice[r][(c+1) % N]+squareLattice[r][(c-1) % N])+squareLattice[r][c]*h
    if energy < 0:
        #if calculated energy is less than 0, flip the spin
        squareLattice[r][c] *= -1
+       current_flip = True
    else:
        #if calculate energy is greater than 0, we flip the spin based on a temperature-dependent probability
        threshhold = (math.e)**(-energy/T)
        n = random.random()
        if n < threshhold:
            squareLattice[r][c] *= -1
+           current_flip = True
+   flipped2 = current_flip
 
-   img.set_data(squareLattice)
+   #img.set_data(squareLattice)
    title_text.set_text(f"Ising Model Simulation | Step: {i}")
-   fig.canvas.draw_idle()
-   plt.pause(0.0001)
+   #fig.canvas.draw_idle()
+   #plt.pause(0.0001)
 
-
-plt.ioff()
+img.set_data(squareLattice)
+#plt.ioff()
 plt.show()
